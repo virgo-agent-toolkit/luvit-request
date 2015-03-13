7e069e67e379a759a39af580aff1b7d858fee27c
Ryan Phillips <ryan.phillips@rackspace.com>
2015-03-13 17:08:09 -0500

    bump to 0.2.2

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "virgo-agent-toolkit/request",
-  version = "0.2.1",
+  version = "0.2.2",
   description = "Rackspace Monitoring Client",
   author = "Ryan Phillips <ryan.phillips@rackspace.com>",
   dependencies = {
