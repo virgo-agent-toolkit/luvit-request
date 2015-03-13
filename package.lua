238260d3d835fb71e1bbb39d8db3c7efb3ea6644
Ryan Phillips <ryan.phillips@rackspace.com>
2015-03-13 17:07:52 -0500

    bump to luvit 2.0.1

--- package.lua
+++ package.lua
@@ -4,7 +4,7 @@ return {
   description = "Rackspace Monitoring Client",
   author = "Ryan Phillips <ryan.phillips@rackspace.com>",
   dependencies = {
-    "luvit/luvit@2.0.0",
+    "luvit/luvit@2.0.1",
     "rphillips/async@0.0.2",
   },
   files = {
