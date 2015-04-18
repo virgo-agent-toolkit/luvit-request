ce20ebf16026b681e1174bd952e387e753602461
Ryan Phillips <ryan.phillips@rackspace.com>
2015-04-18 14:19:27 -0500

    bump deps

--- package.lua
+++ package.lua
@@ -1,10 +1,10 @@
 return {
   name = "virgo-agent-toolkit/request",
-  version = "1.0.0",
+  version = "1.0.1",
   description = "Rackspace Monitoring Client",
   author = "Ryan Phillips <ryan.phillips@rackspace.com>",
   dependencies = {
-    "luvit/luvit@2.0.2",
+    "luvit/luvit@2",
     "virgo-agent-toolkit/async@1",
   },
   files = {
