c924f32744458757af38ffe024279dbf184376ff
Ryan Phillips <ryan.phillips@rackspace.com>
2015-04-18 14:00:47 -0500

    bump to 1.0 request module

--- package.lua
+++ package.lua
@@ -1,11 +1,11 @@
 return {
   name = "virgo-agent-toolkit/request",
-  version = "0.2.4",
+  version = "1.0.0",
   description = "Rackspace Monitoring Client",
   author = "Ryan Phillips <ryan.phillips@rackspace.com>",
   dependencies = {
     "luvit/luvit@2.0.2",
-    "rphillips/async@0.0.2",
+    "virgo-agent-toolkit/async@1",
   },
   files = {
     "**.lua"
